f459d2e42c18bc816fdaa4ce0be8b6ab43d8916c
Balea Raluca <raluca.balea02@e-uvt.ro>
2021-04-11 03:35:24 +0300

    Lista Formulare
    + structura date pentru Formular
    + structura date pentru lista de formulare
    + functie adaugare formular in lista
    + functie citire lista de la tastatura
    + functie de testare - afisarea formularelor

--- Aplicatie_Admitere_Facultate_AlaBun/main.c
+++ Aplicatie_Admitere_Facultate_AlaBun/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define FISIER_UNIFACSPEC "Uni.Fac.Spec.txt"
+#define FISIER_UNIFACSPEC "Univ.Fac.Spec.txt"
 
 
-/** INCARCARE DATE UNIV, FAC, SPEC **/
+/** Structuri date si functii pentru liste univ fac spec **/
 typedef struct{
     char denumire[50];
 }Specializare;
@@ -84,12 +84,129 @@ void afisareUniversitati(Universitate* listaUniversitati, int nrUniversitati) {
     }
 }
 
+/** Structuri date si funciti pt lista formulare **/
+typedef struct {
+    char nume[30];
+    char prenume[50];
+    int varsta;
+    char liceulAbsolvit[50];
+    double medieBac;
+    char* universitate;
+    char* facultate;
+    char* specializare;
+} Formular;
+
+typedef struct NodFormular{
+    Formular f;
+    struct NodFormular* next;
+} NodFormular;
+
+NodFormular* introducereFormular(NodFormular* listaFormulare, Formular form) {
+    NodFormular* nou;
+    nou = malloc(sizeof(NodFormular));
+    nou->f = form;
+    nou->next = NULL;
+
+    if(listaFormulare==NULL) {
+        listaFormulare = nou;
+        return listaFormulare;
+    }
+
+    NodFormular* aux = listaFormulare;
+    while(aux->next!=NULL) aux = aux->next;
+    aux->next = nou;
+    return listaFormulare;
+}
+
+NodFormular* citireFormular(NodFormular* listaFormulare, Universitate* listaUniversitati, int nrUniversitati) {
+    char buffer[100];
+    int indexUni, indexFac, indexSpec;
+
+    Formular form;
+
+    printf("Dati nume: ");
+    fgets(buffer, 100, stdin);
+    buffer[strlen(buffer)-1] = '\0';
+    strcpy(form.nume, buffer);
+
+    printf("Dati prenume: ");
+    fgets(buffer, 100, stdin);
+    buffer[strlen(buffer)-1] = '\0';
+    strcpy(form.prenume, buffer);
+
+    printf("Dati varsta: ");
+    fgets(buffer, 100, stdin);
+    sscanf(buffer, "%d", &form.varsta);
+
+    printf("Liceul absolvit: ");
+    fgets(buffer, 100, stdin);
+    buffer[strlen(buffer)-1] = '\0';
+    strcpy(form.liceulAbsolvit, buffer);
+
+    printf("Dati media de la bac: ");
+    fgets(buffer, 100, stdin);
+    sscanf(buffer, "%lf", &form.medieBac);
+
+    for(int i=0; i<nrUniversitati; i++) {
+        printf("%d. %s\n", i+1, listaUniversitati[i].denumire);
+    }
+    printf("Dati numar universitate: ");
+    fgets(buffer, 100, stdin);
+    sscanf(buffer, "%d", &indexUni);
+    indexUni--;
+    printf("\n");
+
+    for(int i=0; i<listaUniversitati[indexUni].nrFacultati; i++) {
+        printf("%d. %s\n", i+1, listaUniversitati[indexUni].listaFacultati[i].denumire);
+    }
+    printf("Dati numar facultate: ");
+    fgets(buffer, 100, stdin);
+    sscanf(buffer, "%d", &indexFac);
+    indexFac--;
+    printf("\n");
+
+    for(int i=0; i<listaUniversitati[indexUni].listaFacultati[indexFac].nrSpecializari; i++) {
+        printf("%d. %s\n", i+1, listaUniversitati[indexUni].listaFacultati[indexFac].listaSpecializari[i].denumire);
+    }
+    printf("Dati numar specializare: ");
+    fgets(buffer, 100, stdin);
+    sscanf(buffer, "%d", &indexSpec);
+    indexSpec--;
+    printf("\n");
+
+    form.universitate = malloc(strlen(listaUniversitati[indexUni].denumire)*sizeof(char));
+    strcpy(form.universitate, listaUniversitati[indexUni].denumire);
+
+    form.facultate = malloc(strlen(listaUniversitati[indexUni].listaFacultati[indexFac].denumire) * sizeof(char));
+    strcpy(form.facultate, listaUniversitati[indexUni].listaFacultati[indexFac].denumire);
+
+    form.specializare = malloc(strlen(listaUniversitati[indexUni].listaFacultati[indexFac].listaSpecializari[indexSpec].denumire) * sizeof(char));
+    strcpy(form.specializare, listaUniversitati[indexUni].listaFacultati[indexFac].listaSpecializari[indexSpec].denumire);
+
+    listaFormulare = introducereFormular(listaFormulare, form);
+}
+
+void afisareFormulare(NodFormular* listaFormulare) {
+    while(listaFormulare!=NULL) {
+        Formular form = listaFormulare->f;
+        printf("%s %s\n%d\n%s\n%g\n", form.nume, form.prenume, form.varsta, form.liceulAbsolvit, form.medieBac);
+        printf("%s\n%s\n%s\n\n", form.universitate, form.facultate, form.specializare);
+        listaFormulare = listaFormulare->next;
+    }
+}
+
 int main()
 {
     /** INCARCARE UNIV FAC SPEC **/
     Universitate *listaUniversitati;
     int nrUniversitati = 0;
     listaUniversitati = citireUniversitati(listaUniversitati, &nrUniversitati);
+
+    /** Lista Formulare **/
+    NodFormular* listaFormulare = NULL;
+    listaFormulare = citireFormular(listaFormulare, listaUniversitati, nrUniversitati);
+    listaFormulare = citireFormular(listaFormulare, listaUniversitati, nrUniversitati);
+    afisareFormulare(listaFormulare);
 
 
     return 0;
